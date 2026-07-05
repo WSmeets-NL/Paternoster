67a3d279faf8f9278637d5aaebc3dc8ffb169516
Willem Smeets (2505307) <2505307smeets@zuyd.nl>
2026-07-05 10:09:53 +0200

    added delete-functionality to delete parts

---
+++ Paternoster/Pages/DeletePages/DeletePart.cshtml.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Paternoster.Models;
+using Paternoster.DAL;
+
+namespace Paternoster.Pages.DeletePages;
+
+public class DeletePartModel : PageModel
+{
+    private readonly PaternosterDbContext _context;
+
+    public PaternosterContainer PartContainer { get; set; }
+    public List<ProductPart> ProductParts { get; set; } = new List<ProductPart>();
+
+    public DeletePartModel(PaternosterDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Part Part { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var part = await _context.Parts.FirstOrDefaultAsync(m => m.Id == id);
+        if (part is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            Part = part;
+            PartContainer = await _context.PaternosterContainers.Where(c => c.PartId == part.Id).FirstOrDefaultAsync();
+            ProductParts.AddRange(_context.ProductParts.ToList().Where(pp => pp.PartId == Part.Id));
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var part = await _context.Parts.FindAsync(id);
+        if (part != null)
+        {
+            
+            Part = part;
+            _context.Parts.Remove(Part);
+            PartContainer.PartAmount = 0;
+            PartContainer.PartId = null;
+            foreach(ProductPart productPart in ProductParts)
+            {
+                _context.ProductParts.Remove(productPart);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
+    }
+}
