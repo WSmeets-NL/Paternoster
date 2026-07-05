d76f93ae935e8261a57905ea7ddf266f21e47394
Willem Smeets (2505307) <2505307smeets@zuyd.nl>
2026-07-05 10:18:18 +0200

    added delete-functionality for products

---
+++ Paternoster/Pages/DeletePages/DeleteProduct.cshtml.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Paternoster.Models;
+using Paternoster.DAL;
+
+namespace Paternoster.Pages.DeletePages;
+
+public class DeleteProductModel : PageModel
+{
+    private readonly PaternosterDbContext _context;
+
+    public List<ProductPart> ProductParts { get; set; } = new List<ProductPart>();
+    public DeleteProductModel(PaternosterDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Product Product { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            Product = product;
+            ProductParts.AddRange(_context.ProductParts.ToList().Where(pp => pp.ProductId == Product.Id));
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var product = await _context.Products.FindAsync(id);
+        if (product != null)
+        {
+            Product = product;
+            _context.Products.Remove(Product);
+            foreach(ProductPart productPart in ProductParts)
+            {
+                _context.ProductParts.Remove(productPart);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("/Products");
+    }
+}
